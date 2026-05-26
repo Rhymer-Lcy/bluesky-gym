8d5eef80e44eceda17cf91dd037b32da0e4654ad
Rhymer-Lcy <1014917866@qq.com>
2026-05-26 23:47:15 +0800

    fix(polygon-generator): move module-level demo code into __main__ guard
    All executable code after the function definitions (PIL image rendering,
    numpy sector bitmap and matplotlib imshow) ran unconditionally on import.
    Wrapped in 'if __name__ == __main__:' and moved PIL/numpy/matplotlib
    imports inside the guard so they are only needed when run directly.

--- bluesky_gym/envs/common/polygon_generator.py
+++ bluesky_gym/envs/common/polygon_generator.py
@@ -1,8 +1,6 @@
-import math, random
+import math
+import random
 from typing import List, Tuple
-from PIL import Image, ImageDraw
-import numpy as np
-import matplotlib.pyplot as plt
 
 def generate_polygon(center: Tuple[float, float], avg_radius: float,
                      irregularity: float, spikiness: float,
@@ -82,45 +80,39 @@ def random_angle_steps(steps: int, irregularity: float) -> List[float]:
         angles[i] /= cumsum
     return angles
 
-def clip(value, lower, upper):
-    """
-    Given an interval, values outside the interval are clipped to the interval
-    edges.
-    """
+def clip(value: float, lower: float, upper: float) -> float:
+    """Clamp *value* to the closed interval [*lower*, *upper*]."""
     return min(upper, max(value, lower))
 
-vertices = generate_polygon(center=(250, 250),
-                            avg_radius=100,
-                            irregularity=0.35,
-                            spikiness=0.2,
-                            num_vertices=16)
 
+if __name__ == '__main__':
+    from PIL import Image, ImageDraw
+    import numpy as np
+    import matplotlib.pyplot as plt
 
-black = (0, 0, 0)
-white = (255, 255, 255)
-img = Image.new('RGB', (500, 500), white)
-im_px_access = img.load()
-draw = ImageDraw.Draw(img)
+    vertices = generate_polygon(center=(250, 250),
+                                avg_radius=100,
+                                irregularity=0.35,
+                                spikiness=0.2,
+                                num_vertices=16)
 
-# either use .polygon(), if you want to fill the area with a solid colour
-draw.polygon(vertices, outline=black, fill=black)
+    black = (0, 0, 0)
+    white = (255, 255, 255)
+    img = Image.new('RGB', (500, 500), white)
+    draw = ImageDraw.Draw(img)
 
-# or .line() if you want to control the line thickness, or use both methods together!
-draw.line(vertices + [vertices[0]], width=2, fill=black)
-img.show()
+    draw.polygon(vertices, outline=black, fill=black)
+    draw.line(vertices + [vertices[0]], width=2, fill=black)
+    img.show()
 
-sector_array = np.asarray(img)
+    sector_array = np.asarray(img)
+    sector_bitmap = np.zeros((np.size(sector_array, 0), np.size(sector_array, 1)))
+    for row in range(np.size(sector_array, 0)):
+        for column in range(np.size(sector_array, 1)):
+            if np.array_equal(sector_array[row, column, :], np.array([255, 255, 255])):
+                sector_bitmap[row, column] = 0
+            else:
+                sector_bitmap[row, column] = 1
 
-sector_bitmap = np.zeros((np.size(sector_array, 0), np.size(sector_array, 1)))
-
-
-for row in range(np.size(sector_array, 0)):
-    for column in range(np.size(sector_array, 1)):
-        if np.array_equal(sector_array[row, column, :], np.array([255, 255, 255])):
-            sector_bitmap[row, column] = 0
-        else:
-            sector_bitmap[row, column] = 1
-
-# plt.plot(sector_bitmap)
-# plt.show()
-plt.imshow(sector_bitmap,cmap='gray_r')
+    plt.imshow(sector_bitmap, cmap='gray_r')
+    plt.show()
